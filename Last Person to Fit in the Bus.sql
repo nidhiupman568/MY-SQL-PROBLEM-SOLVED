47aae4302cf62a708ff8a720d78a05b4bd4cb4a7
NIDHI UPMAN <130860182+nidhiupman568@users.noreply.github.com>
2024-06-21 20:59:32 +0530

    Create Last Person to Fit in the Bus.sql

---
+++ Last Person to Fit in the Bus.sql
@@ -0,0 +1,12 @@
+# Find the maximum number of turns among rows where 'tot_weight <= 1000'.
+
+WITH CTE AS (
+    SELECT 
+        turn, person_name, weight,
+        SUM(weight) OVER(ORDER BY turn ASC) AS tot_weight 
+    FROM Queue
+    ORDER BY turn
+)
+SELECT person_name
+FROM Queue q
+WHERE q.turn = (SELECT MAX(turn) FROM CTE WHERE tot_weight <= 1000);
